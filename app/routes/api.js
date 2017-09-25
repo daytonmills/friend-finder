74b060eaec490b2fd02144f17bd9cc8ff132babe
Dayton Mills <dayton@statscraft.net>
2017-09-25 11:53:52 -0700

    Project Structure

---
+++ app/routes/api.js
@@ -0,0 +1,14 @@
+const friendData = require('../data/friends');
+
+module.exports = function(app)
+{
+    app.get('/api/friends', (req, res) =>
+    {
+        //
+    });
+
+    app.post('/api/friends', (req, res) =>
+    {
+        //
+    });
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+require('./routes/api')(app);
+require('./routes/html')(app);
+
+app.listen(PORT, function()
+{
+    console.log('App listening on PORT: ' + PORT);
+});
